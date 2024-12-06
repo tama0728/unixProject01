355223753dcaaaee0e8e2f8233725eb67f9f145e
Kim Dongyun <edw0728@gmail.com>
2024-12-07 03:26:49 +0900

    add server & client terminal

--- local/gameLogic.h
+++ local/gameLogic.h
@@ -1,27 +1,22 @@
 #ifndef GAME_LOGIC_H
 #define GAME_LOGIC_H
 
-#include <glib.h>
-
-static char input_buffer[11] = ""; // 최대 길이 10
-static int gameStatus = 0;
-
 // 문자열 길이 설정 및 가져오기
-void set_solution(const char *input);
 void set_string_length(int length);
 int get_string_length();
+void set_solution(const char *input);
 void set_input(const char *input);
+int get_game_status();
+void clear_game_status();
 
 // 입력 검증 및 추가
 int validate_input(const char *input);
-void add_input(const char *current_text, const char *new_char, gpointer entry);
+void add_input(const char *current_text, const char *new_char);
 const char *get_current_input();
 
 // 게임 상태
 int is_game_complete();
 const char* numBaseball();
-void reset_input();
-int get_game_status();
-void clear_game_status();
+void reset_game();
 
 #endif // GAME_LOGIC_H
