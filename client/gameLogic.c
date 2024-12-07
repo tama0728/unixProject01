97b14ac33b44a005d2cb4709c515bfb23b6be355
Kim Dongyun <edw0728@gmail.com>
2024-12-07 14:24:50 +0900

    remove does not use code

--- client/gameLogic.c
+++ client/gameLogic.c
@@ -1,16 +1,9 @@
 #include "gameLogic.h"
 #include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-//#include <gtk/gtk.h>
-#include <ctype.h>
 
 static char input_buffer[50] = ""; // 최대 길이 10
 static int string_length = 0;
-static char solution[11] = "64382";
-static char result[50];
-static int gameCount = 0;
-static int gameStatus = 0;
 
 void set_string_length(int length) {
     string_length = length;
@@ -23,38 +16,10 @@ int get_string_length() {
 const char *get_current_input() {
     return input_buffer;
 }
-
-void set_solution(const char *input) {
-    strcpy(solution, input);
-    set_string_length(strlen(input));
-}
-
-void set_input(const char *input) {
-    strcpy(input_buffer, input);
-}
-
-int get_game_status() {
-    return gameStatus;
-}
-
-void clear_game_status() {
-    gameStatus = 0;
-}
-
-int validate_input(const char *input) {
-    for (int i = 0; input[i] != '\0'; ++i) {
-        if (!isdigit(input[i])) return 0;
-    }
-    int length = atoi(input);
-    return (length > 0 && length < 10) ? length : 0;
-}
-
 void add_input(const char *current_text, const char *new_char) {
     if (strlen(current_text) >= get_string_length()) {
-//        g_print("문자열 길이를 초과했습니다. (입력값: %s)\n", current_text);
         reset_input();
         strncat(input_buffer, new_char, 1);
-//        gtk_editable_set_text(GTK_EDITABLE(entry), new_char); // 입력 창 초기화
         return;
     }
 
@@ -66,44 +31,10 @@ void add_input(const char *current_text, const char *new_char) {
     }
 
     strncat(input_buffer, new_char, 1);
-//    gtk_editable_set_text(GTK_EDITABLE(entry), input_buffer); // 입력 창 초기화
 }
 
 int is_game_complete() {
     return strlen(input_buffer) == string_length;
-}
-
-const char* numBaseball() {
-    int strike = 0, ball = 0;
-
-    if (strlen(input_buffer) != string_length) {
-        return 0;
-    }
-
-    for (int i = 0; i < string_length; ++i) {
-        for (int j = 0; j < string_length; ++j) {
-            if (solution[i] == input_buffer[j]) {
-                if (i == j) {
-                    strike++;
-                } else {
-                    ball++;
-                }
-            }
-        }
-    }
-    sprintf(result, "%d: Strike: %d, Ball: %d\n", gameCount, strike, ball);
-    gameCount++;
-    if (strike == string_length) {
-        printf("You win!\n");
-        gameStatus = 1;
-        return "You win!\n";
-    } else if (gameCount >= 9) {
-        sprintf(result, "You lose! The answer was %s\n", solution);
-        gameStatus = 1;
-        return result;
-    } else {
-        return result;
-    }
 }
 
 void reset_input() {
